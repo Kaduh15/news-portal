d5b16089504e3cf27256e44c0f8d512b625276ff
Kadu <kadu.silva2014@gmail.com>
2024-06-10 18:57:37 -0300

    Feat: adiciona botão para mudar o tema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import Article from '@/components/Article'
-import SelectApi from '@/components/SelectApi'
+import { ModeToggle } from '@/components/ui/mode-toggle'
 import { createApi } from '@/helper/api'
 
 export default async function Home() {
-  const api = await createApi('gnews')
+  const api = await createApi('news')
 
   const result = await api.getArticles()
 
   return (
     <>
-      <header className="w-full border-b border-zinc-300 p-8">
+      <header className="flex w-full items-center justify-between border-b border-zinc-300 p-8">
         <h1 className="text-3xl font-bold">Latest News</h1>
-        <SelectApi />
+        <ModeToggle />
       </header>
 
       <main className="mt-8 flex flex-wrap justify-evenly gap-x-5 px-5">
